c763be3814fb971844ab7f1f12b336f72ce5b5cc
Oscar Ballesteros <ext_orballesteros@falabella.cl>
2021-06-28 16:45:29 -0400

    refact pacmen.js

--- pacmen.js
+++ pacmen.js
@@ -1,7 +1,7 @@
 var pos = 0;
 const pacArray = [
-    ['PacMan1.png', 'PacMan2.png'],
-    ['PacMan3.png', 'PacMan4.png']
+    ['./images/PacMan1.png', './images/PacMan2.png'],
+    ['./images/PacMan3.png', './images/PacMan4.png']
 ];
 var direction = 0;
 const pacMen = []; // This array holds all the pacmen
@@ -17,17 +17,18 @@ function makePac() {
     // returns an object with random values scaled {x: 33, y: 21}
     let velocity = setToRandom(10); // {x:?, y:?}
     let position = setToRandom(200);
+    console.log(position)
     // Add image to div id = game
     let game = document.getElementById('game');
     let newimg = document.createElement('img');
     newimg.style.position = 'absolute';
-    newimg.src = 'PacMan1.png';
+    newimg.src = './images/PacMan1.png';
     newimg.width = 100;
     newimg.height = 100;
     //
     // set position here 
-    newimg.style.left = position.x;
-    newimg.style.top = position.y;
+    newimg.style.left = position.x + 'px';
+    newimg.style.top = position.y + 'px';
     //
 
 
@@ -47,23 +48,27 @@ function update() {
         checkCollisions(item)
         item.position.x += item.velocity.x;
         item.position.y += item.velocity.y;
+        console.log(item.position)
 
-        item.newimg.style.left = item.position.x;
-        item.newimg.style.top = item.position.y;
+        item.newimg.style.left = item.position.x + 'px';
+        item.newimg.style.top = item.position.y + 'px';
     })
-    setTimeout(update, 20);
+    console.log("update!")
+    setTimeout(update, 10);
 }
 
 function checkCollisions(item) {
-    console.log(item.position.x, item.newimg.width);
     if(item.position.x + item.newimg.width > window.innerWidth || item.position.x < 0)
         item.velocity.x = -item.velocity.x;
     if(item.position.y + item.newimg.height > window.innerHeight || item.position.y < 0)
         item.velocity.y = -item.velocity.y;   
-
-
     }
 
 function makeOne() {
     pacMen.push(makePac()); // add a new PacMan
 }
+
+//don't change this line
+if (typeof module !== 'undefined') {
+    module.exports = { checkCollisions, update, pacMen };
+  }
